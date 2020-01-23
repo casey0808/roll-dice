bfee60a1b4b962af6c48db8d78a29ca5fc138e21
casey0808 <casey_c0808@outlook.com>
2020-01-23 14:07:45 +0800

    Updates

---
+++ precache-manifest.a5f7b71329eba14ee8107563a1a9d148.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9091f989b9031166e980b66a4e381c",
+    "url": "/roll-dice/index.html"
+  },
+  {
+    "revision": "c4be486eb09a692ca118",
+    "url": "/roll-dice/static/css/main.e5eec234.chunk.css"
+  },
+  {
+    "revision": "4034f56bde0cc95223d5",
+    "url": "/roll-dice/static/js/2.4a450a10.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/roll-dice/static/js/2.4a450a10.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4be486eb09a692ca118",
+    "url": "/roll-dice/static/js/main.ed0a4b31.chunk.js"
+  },
+  {
+    "revision": "eca473a9514a92367209",
+    "url": "/roll-dice/static/js/runtime-main.c96f41c9.js"
+  }
+]);
